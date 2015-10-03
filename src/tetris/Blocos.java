7e39598f91ebd599cad0a617e8dba35f4fa8a0fe
Peter Clayder <peterclayder@gmail.com>
2015-10-03 01:35:15 -0300

    acabei sexta

--- src/tetris/Blocos.java
+++ src/tetris/Blocos.java
@@ -17,48 +17,150 @@ public class Blocos {
 
     private void criaBlocos() {
 
-        this.blocoL(); // cria um bloco L
-        
+        //this.blocoJ(20, 0, 530, rotacao); // cria um bloco L
         blocos = new ArrayList<Objeto>();
-        
-        blocos.add(new Objeto(this.obj0.get(0), this.obj0.get(1), "bloco1.png"));
-        blocos.add(new Objeto(this.obj1.get(0), this.obj1.get(1), "bloco1.png"));
-        blocos.add(new Objeto(this.obj2.get(0), this.obj2.get(1), "bloco1.png"));
-        blocos.add(new Objeto(this.obj3.get(0), this.obj3.get(1), "bloco1.png"));
 
+        //blocos.add(new Objeto(this.obj0.get(0), this.obj0.get(1), "bloco1.png"));
+        //blocos.add(new Objeto(this.obj1.get(0), this.obj1.get(1), "bloco2.png"));
+        //blocos.add(new Objeto(this.obj2.get(0), this.obj2.get(1), "bloco3.png"));
+        //blocos.add(new Objeto(this.obj3.get(0), this.obj3.get(1), "bloco5.png"));
+        Objeto bloco1 = new Objeto(200, 0, "bloco1.png");
+        Objeto bloco2 = new Objeto(220, 0, "bloco2.png");
+        Objeto bloco3 = new Objeto(240, 0, "bloco3.png");
+        Objeto bloco4 = new Objeto(240, 20, "bloco5.png");
+
+        blocos = new ArrayList<Objeto>();
+
+        blocos.add(bloco1);
+
+        blocos.add(bloco2);
+
+        blocos.add(bloco3);
+        blocos.add(bloco4);
+
+        
+                 blocos.get(0).setFloor(530);
+
+         blocos.get(1).setFloor(530);
+
+         blocos.get(2).setFloor(530);
+
+         blocos.get(3).setFloor(550);
+         /*
         blocos.get(0).setFloor(this.obj0.get(2));
         blocos.get(1).setFloor(this.obj1.get(2));
         blocos.get(2).setFloor(this.obj2.get(2));
         blocos.get(3).setFloor(this.obj3.get(2));
+         */
+
     }
 
-    private void blocoL() {
-        
-        int x = 200;
-        int y = 0;
-        int limite = 530;
-        
+    private void blocoJ(int x, int y, int limite, int rotacao) {
+        List<Integer> objRot0 = new ArrayList<Integer>();
+        List<Integer> objRot1 = new ArrayList<Integer>();
+        List<Integer> objRot2 = new ArrayList<Integer>();
+        List<Integer> objRot3 = new ArrayList<Integer>();
+
+        /*
+         *   ---|
+         */
+        if (rotacao == 0) {
+            objRot0.add(0); // x
+            objRot0.add(0); // y
+            objRot0.add(0); // limite
+
+            objRot1.add(20);
+            objRot1.add(0);
+            objRot1.add(0);
+
+            objRot2.add(40);
+            objRot2.add(0);
+            objRot2.add(0);
+
+            objRot3.add(40);
+            objRot3.add(20);
+            objRot3.add(20);
+        } /*
+         * J
+         */ else if (rotacao == 1) {
+            objRot0.add(20);
+            objRot0.add(0);
+            objRot0.add(0);
+
+            objRot1.add(20);
+            objRot1.add(20);
+            objRot1.add(0);
+
+            objRot2.add(20);
+            objRot2.add(40);
+            objRot2.add(0);
+
+            objRot3.add(0);
+            objRot3.add(40);
+            objRot3.add(20);
+        } /*
+         * |---
+         */ else if (rotacao == 2) {
+            objRot0.add(0);
+            objRot0.add(20);
+            objRot0.add(20);
+
+            objRot1.add(40);
+            objRot1.add(40);
+            objRot1.add(0);
+
+            objRot2.add(20);
+            objRot2.add(40);
+            objRot2.add(0);
+
+            objRot3.add(0);
+            objRot3.add(40);
+            objRot3.add(20);
+        } /*
+         *  _
+         |
+         |
+         */ else if (rotacao == 3) {
+            objRot0.add(20);
+            objRot0.add(40);
+            objRot0.add(20);
+
+            objRot1.add(0);
+            objRot1.add(80);
+            objRot1.add(0);
+
+            objRot2.add(0);
+            objRot2.add(60);
+            objRot2.add(0);
+
+            objRot3.add(0);
+            objRot3.add(40);
+            objRot3.add(20);
+        }
+
         this.obj0 = new ArrayList<Integer>();
-        this.obj0.add(x); // Add o valor da posicao x
-        this.obj0.add(y); // Add o valor da posicao y
-        this.obj0.add(limite); // Add o limite
-        
+        this.obj0.add(x + objRot0.get(0)); // Add o valor da posicao x
+        this.obj0.add(y + objRot0.get(1)); // Add o valor da posicao y
+        this.obj0.add(limite + objRot0.get(2)); // Add o limite
+
         this.obj1 = new ArrayList<Integer>();
-        this.obj1.add(x + 20); // Add o valor da posicao x
-        this.obj1.add(y); // Add o valor da posicao y
-        this.obj1.add(limite); // Add o limite
-        
+        this.obj1.add(x + objRot1.get(0)); // Add o valor da posicao x
+        this.obj1.add(y + objRot1.get(1)); // Add o valor da posicao y
+        this.obj1.add(limite + objRot1.get(2)); // Add o limite
+
         this.obj2 = new ArrayList<Integer>();
-        this.obj2.add(x + 40); // Add o valor da posicao x
-        this.obj2.add(y); // Add o valor da posicao y
-        this.obj2.add(limite); // Add o limite
-        
+        this.obj2.add(x + objRot2.get(0)); // Add o valor da posicao x
+        this.obj2.add(y + objRot2.get(1)); // Add o valor da posicao y
+        this.obj2.add(limite + objRot2.get(2)); // Add o limite
+
         this.obj3 = new ArrayList<Integer>();
-        this.obj3.add(x + 40); // Add o valor da posicao x
-        this.obj3.add(y + 20); // Add o valor da posicao y
-        this.obj3.add(limite + 20); // Add o limite
+        this.obj3.add(x + objRot3.get(0)); // Add o valor da posicao x
+        this.obj3.add(y + objRot3.get(1)); // Add o valor da posicao y
+        this.obj3.add(limite + objRot3.get(2)); // Add o limite
     }
 
+    //public List<Objeto> rotacionarJ(int x, int y) {
+    //}
     public List<Objeto> getBlocos() {
         return blocos;
     }
--- src/tetris/Cenario.java
+++ src/tetris/Cenario.java
@@ -5,6 +5,7 @@
  */
 package tetris;
 
+import jplay.Keyboard;
 import jplay.Scene;
 import jplay.Sprite;
 import jplay.Window;
@@ -18,20 +19,27 @@ public class Cenario {
     private Window janela;
     private Scene cena;
     Blocos blocos;
+    Blocos blocosTeste;
+    int rotacao = 0;
+    Keyboard teclado;
 
     public Cenario(Window window) {
         janela = window;
+        teclado = janela.getKeyboard();
         cena = new Scene();
         cena.loadFromFile("cenario.scn");
         cena.setDrawStartPos(0, 0); // posiciona o cenario
         /*
-        * Vai dar o ponta pé inicial criando um bloco com 4 objetos
-        */
+         * Vai dar o ponta pé inicial criando um bloco com 4 objetos
+         */
         blocos = new Blocos();
+        //blocosTeste = new Blocos(1);
         /*
-        * Retorna uma lista contendo os 4 objetos
-        */
+         * Retorna uma lista contendo os 4 objetos
+         */
         blocos.getBlocos();
+
+        // blocosTeste.getBlocos();
         run();
     }
 
@@ -40,21 +48,35 @@ public class Cenario {
 
             cena.draw();
             /*
-            * Imprime o bloco
-            */
+             * Imprime o bloco
+             */
             for (Sprite sp : blocos.getBlocos()) {
                 sp.draw();
                 sp.fall();
             }
 
-            /*
-            * Assim que os 4 objetos chegarem ao seu destino outros 4 vão ser criados 
-            */
-            if (blocos.getBlocos().get(0).isOnFloor() && blocos.getBlocos().get(1).isOnFloor() && blocos.getBlocos().get(2).isOnFloor() && blocos.getBlocos().get(3).isOnFloor()) {
-                blocos = new Blocos();
-                blocos.getBlocos();
+            // Apertar para rotacionar 
+            if (teclado.keyDown(Keyboard.LEFT_KEY)) {
+                blocos.getBlocos().get(0).x = blocos.getBlocos().get(0).x + 20;
             }
 
+
+            /*
+             * Assim que os 4 objetos chegarem ao seu destino outros 4 vão ser criados 
+             */
+            /*
+             if (blocos.getBlocos().get(0).isOnFloor() && blocos.getBlocos().get(1).isOnFloor() && blocos.getBlocos().get(2).isOnFloor() && blocos.getBlocos().get(3).isOnFloor()) {
+                
+             rotacao++;
+             if(rotacao > 3){
+             rotacao = 0;
+             }
+               
+             blocos = new Blocos(rotacao);
+             blocos.getBlocos();
+                
+             }
+             */
             janela.update();
         }
     }
